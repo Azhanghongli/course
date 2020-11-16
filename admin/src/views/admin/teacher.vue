ddbb6d1c31687a598ea60ba9840c6ca666147593
delia <1987944423@qq.com>
2020-11-16 11:16:12 +0800

    文件上传功能开发：文件上传成功后返回值处理

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -235,7 +235,7 @@
 
       afterUpload(resp) {
         let _this = this;
-        let image = resp.content;
+        let image = resp.content.path;
         _this.teacher.image = image;
       }
     }
